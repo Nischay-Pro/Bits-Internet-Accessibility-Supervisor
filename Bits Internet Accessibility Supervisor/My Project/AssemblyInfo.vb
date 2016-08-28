e56bbeb1333854785433c06b2c2a2a19e09d7677
Nischay Pro <NischayPro@yahoo.co.in>
2016-08-28 23:37:37 +0530

    Clean Up Commit

--- Bits Internet Accessibility Supervisor/My Project/AssemblyInfo.vb
+++ Bits Internet Accessibility Supervisor/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.301.6084")>
-<Assembly: AssemblyFileVersion("1.0.301.6084")>
+<Assembly: AssemblyVersion("1.0.302.6085")>
+<Assembly: AssemblyFileVersion("1.0.302.6085")>
